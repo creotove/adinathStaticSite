2db05d5c912c8edceb98b4c4d3bfccd17e03b2d4
1altamas <67779413+1altamas@users.noreply.github.com>
2023-10-21 12:15:27 +0530

    added frontend forget to removed in git ignore file

---
+++ frontEnd/build/static/js/1890.8e6b4203.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_spellgrowth_adi_nath_group=self.webpackChunk_spellgrowth_adi_nath_group||[]).push([[1890],{51890:function(e,n,r){r.r(n);r(72791);var t=r(80184);n.default=function(){return(0,t.jsx)("div",{children:"Index"})}}}]);
+//# sourceMappingURL=1890.8e6b4203.chunk.js.map
